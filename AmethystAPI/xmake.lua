dced7efad7952d6ed8bdfbbc2932be9d91b2fa2f
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-08-30 10:01:21 -0300

    Add ContainerScreenController and container enum
    Introduced ContainerEnumName enum and ContainerScreenController class. Updated xmake.lua to use LOCALAPPDATA for libhat include path, improving portability.

--- AmethystAPI/xmake.lua
+++ AmethystAPI/xmake.lua
@@ -50,7 +50,9 @@ target("AmethystAPI")
     -- Deps
     add_packages("libhat")
 
-    add_includedirs("C:/Users/blake/AppData/Local/.xmake/packages/l/libhat/@default/3c332be551f6485e8d17e1830ee49789/include")
+    local localAppData = os.getenv("LOCALAPPDATA")
+    add_includedirs(
+        path.join(localAppData, ".xmake/packages/l/libhat/@default/3c332be551f6485e8d17e1830ee49789/include"))
     add_includedirs("include", "src", {public = true})
 
     add_headerfiles("src/**.hpp", "include/**.hpp", "include/**.h")
