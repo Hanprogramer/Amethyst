4022e90673b4a11833904ac3f352552815c13db8
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-08-30 14:35:41 +0100

    Fix old path

--- AmethystAPI/src/minecraft/src-client/common/client/gui/screens/controllers/ContainerScreenController.hpp
+++ AmethystAPI/src/minecraft/src-client/common/client/gui/screens/controllers/ContainerScreenController.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <unordered_map>
 #include <string>
-#include "minecraft/src-client/common/client/gui/screens/controllers/ContainerEnumName.hpp"
+#include "minecraft/src/common/world/containers/ContainerEnumName.hpp"
 
 #pragma pack(push, 1)
 class ContainerScreenController /** : ClientInstanceScreenController, MinecraftScreenController, ScreenController, IScreenController, std::enable_shared_from_this<MinecraftScreenController> **/ {
