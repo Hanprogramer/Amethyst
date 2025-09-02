739f6bf3c834f83a71bf7e599548f4b1a830e50e
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-09-02 18:19:16 +0100

    Fix messy libhat imports by moving it to a submodule

--- AmethystAPI/xmake.lua
+++ AmethystAPI/xmake.lua
@@ -10,31 +10,7 @@ toolchain("nasm")
     end)
 toolchain_end()
 
-package("libhat")
-    set_kind("static")
-    add_urls("https://github.com/BasedInc/libhat.git")
-    add_versions("commit", "3321c66b7e699e5585948bdb663c105efa48c680")
-    add_deps("cmake")
-
-    on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_CXX_STANDARD=23")
-        table.insert(configs, "-DCMAKE_CXX_STANDARD_REQUIRED=ON")
-        table.insert(configs, "-DCMAKE_CXX_EXTENSIONS=OFF")
-        table.insert(configs, "-DLIBHAT_TESTING=OFF")
-        table.insert(configs, "-DLIBHAT_EXAMPLES=OFF")
-        table.insert(configs, "-DLIBHAT_MODULE_TARGET=OFF")
-        table.insert(configs, "-DLIBHAT_STATIC_C_LIB=OFF")
-        table.insert(configs, "-DLIBHAT_SHARED_C_LIB=OFF")
-        table.insert(configs, "-DLIBHAT_INSTALL_TARGET=ON")
-        table.insert(configs, "-DCMAKE_CXX_FLAGS=/O2 /Zi /DNDEBUG /MD /EHsc /FS")
-
-        import("package.tools.cmake").install(package, configs)
-        os.cp("include", package:installdir())
-    end)
-package_end()
-
-add_requires("libhat", { system = false })
+includes("packages/libhat")
 
 target("AmethystAPI")
     set_kind("static")
@@ -49,17 +25,7 @@ target("AmethystAPI")
     add_files("src/**.asm", { languages = "nasm" })
 
     -- Deps
-    add_packages("libhat")
+    add_deps("libhat")
 
-    -- local localAppData = os.getenv("LOCALAPPDATA")
-    -- add_includedirs(
-    --     path.join(localAppData, ".xmake/packages/l/libhat/@default/3c332be551f6485e8d17e1830ee49789/include")
-    -- )
-
-    add_includedirs(
-        path.join(os.projectdir(), "packages", ".xmake", "packages", "l", "libhat", "@default", "3c332be551f6485e8d17e1830ee49789", "include")
-    )
-    
     add_includedirs("include", "src", {public = true})
-
     add_headerfiles("src/**.hpp", "include/**.hpp", "include/**.h")
--- xmake.lua
+++ xmake.lua
@@ -27,32 +27,6 @@ toolchain("nasm")
 toolchain_end()
 
 -- Project dependencies
-package("libhat")
-    set_kind("static")
-    add_urls("https://github.com/BasedInc/libhat.git")
-    add_versions("commit", "3321c66b7e699e5585948bdb663c105efa48c680")
-    add_deps("cmake")
-
-    on_install(function (package)
-        local configs = {}
-        table.insert(configs, "-DCMAKE_CXX_STANDARD=23")
-        table.insert(configs, "-DCMAKE_CXX_STANDARD_REQUIRED=ON")
-        table.insert(configs, "-DCMAKE_CXX_EXTENSIONS=OFF")
-        table.insert(configs, "-DLIBHAT_TESTING=OFF")
-        table.insert(configs, "-DLIBHAT_EXAMPLES=OFF")
-        table.insert(configs, "-DLIBHAT_MODULE_TARGET=OFF")
-        table.insert(configs, "-DLIBHAT_STATIC_C_LIB=OFF")
-        table.insert(configs, "-DLIBHAT_SHARED_C_LIB=OFF")
-        table.insert(configs, "-DLIBHAT_INSTALL_TARGET=ON")
-        table.insert(configs, "-DCMAKE_CXX_FLAGS=/O2 /Zi /DNDEBUG /MD /EHsc /FS")
-
-        import("package.tools.cmake").install(package, configs)
-        os.cp("include", package:installdir())
-    end)
-package_end()
-
-add_requires("libhat", { system = false })
-
 local amethystFolder = path.join(
     os.getenv("localappdata"),
     "Packages",
@@ -96,11 +70,7 @@ target("AmethystRuntime")
 
     local localAppData = os.getenv("LOCALAPPDATA")
 
-    -- need to figure out how to fix this shit lmao
-    add_links("user32", "oleaut32", "windowsapp", 
-        path.join(localAppData, ".xmake/packages/l/libhat/@default/3c332be551f6485e8d17e1830ee49789/lib/libhat"))
-    add_includedirs(
-        path.join(localAppData, ".xmake/packages/l/libhat/@default/3c332be551f6485e8d17e1830ee49789/include"))
+    add_links("user32", "oleaut32", "windowsapp")
     add_includedirs("src", {public = true})
 
     add_headerfiles("src/**.hpp")
