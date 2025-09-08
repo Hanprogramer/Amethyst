a84a6bf6b235a86e7f6af516a0ca46de284ca20e
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-09-08 19:41:14 +0100

    ScreenController::registerButtonInteractedHandler and WorldStorageHandler

---
+++ AmethystAPI/src/minecraft/src-client/common/client/world/WorldStorageHandler.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <minecraft/src-deps/core/file/Path.hpp>
+#include "minecraft/src-deps/core/utility/NonOwnerPointer.hpp"
+#include <minecraft/src-deps/core/headerIncludes/gsl_includes.hpp>
+
+class ILevelListCache;
+class IContentManager;
+class FileArchiver;
+class LevelDbEnv;
+namespace Social {
+    class IUserManager;
+};
+class HolographicPlatform;
+class IContentKeyProvider;
+
+namespace World {
+
+class WorldStorageHandler {
+public:
+    /// @signature {48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 49 8B E9 4D 8B E0 4C 8B F2}
+    WorldStorageHandler(
+        const Core::PathBuffer<std::string>& a2, 
+        ILevelListCache& a3, 
+        IContentManager& a4, 
+        FileArchiver& a5, 
+        Social::IUserManager& a6,  
+        HolographicPlatform& a7, 
+        const gsl::not_null<Bedrock::NonOwnerPointer<const IContentKeyProvider>>& a8,
+        gsl::not_null<Bedrock::NonOwnerPointer<LevelDbEnv>> a9
+    );
+};
+
+};
