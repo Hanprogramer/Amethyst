c341936acbf22c5fd9c9aabc17cfdbc2b149b5be
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-09-12 15:59:16 -0300

    Added some new functions for binding stuff to screen

--- AmethystAPI/src/minecraft/src/common/world/item/registry/ItemRegistryManager.hpp
+++ AmethystAPI/src/minecraft/src/common/world/item/registry/ItemRegistryManager.hpp
@@ -1,7 +1,9 @@
+/// @symbolgeneration
 #pragma once
 #include "minecraft/src/common/world/item/registry/ItemRegistryRef.hpp"
 
 class ItemRegistryManager {
 public:
+    /// @signature {40 53 48 83 EC ? 48 83 3D ? ? ? ? ? 48 8B D9 74}
     static ItemRegistryRef getItemRegistry();
 };
