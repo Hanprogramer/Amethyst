4473f28d26b0f2921ab00ecea69cb01737c00adc
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-09-14 19:40:50 -0300

    Update SceneFactory.hpp

--- AmethystAPI/src/minecraft/src-client/common/client/gui/screens/SceneFactory.hpp
+++ AmethystAPI/src/minecraft/src-client/common/client/gui/screens/SceneFactory.hpp
@@ -1,10 +1,11 @@
 /// @symbolgeneration
 #pragma once
+#include "amethyst/Imports.hpp"
 #include "minecraft/src-deps/core/utility/NonOwnerPointer.hpp"
 #include "minecraft/src-client/common/client/gui/screens/SceneStack.hpp"
 
 class SceneFactory {
 public:
     /// @signature {48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 48 8B EA 33 F6}
-    Bedrock::NonOwnerPointer<SceneStack> getCurrentSceneStack();
+    MC Bedrock::NonOwnerPointer<SceneStack> getCurrentSceneStack();
 };
