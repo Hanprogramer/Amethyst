e9d12bda3d193d1d4eb2d653c10c42217a5ebd2e
Raony Fernandes dos Reis <raonyreisdev@gmail.com>
2025-10-01 14:42:57 -0300

    Fallbacking to 0.0.0-dev if versioned folder isn't found

---
+++ AmethystAPI/src/mc/src/common/world/item/registry/TrimMaterialRegistry.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <vector>
+#include "mc/src/common/world/item/registry/TrimMaterial.hpp"
+
+class TrimMaterialRegistry {
+public:
+    std::vector<TrimMaterial> mTrimMaterials;
+};
