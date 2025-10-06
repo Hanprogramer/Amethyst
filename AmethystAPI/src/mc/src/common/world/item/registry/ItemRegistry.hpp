46938853601a5f1ad00b74a5c133508fb549fb3b
Raony Fernandes dos Reis <raonyreisdev@gmail.com>
2025-10-06 15:32:09 -0300

    Update ItemRegistry.hpp

--- AmethystAPI/src/mc/src/common/world/item/registry/ItemRegistry.hpp
+++ AmethystAPI/src/mc/src/common/world/item/registry/ItemRegistry.hpp
@@ -31,4 +31,8 @@ public:
 
     /// @signature {48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B EA 4C 8B F1 48 89 4C 24 ? 48 89 55}
     MC void registerItem(SharedPtr<Item> item);
+
+    short getNextItemID() {
+        return ++mMaxItemID;
+    }
 };
