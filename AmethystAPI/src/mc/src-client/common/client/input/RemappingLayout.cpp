752586ac3ab5875607baa5f6fe77e8d9b7f0fd0c
Raony Fernandes dos Reis <raonyreisdev@gmail.com>
2025-10-07 23:25:06 -0300

    Stuff of item info

--- AmethystAPI/src/mc/src-client/common/client/input/RemappingLayout.cpp
+++ AmethystAPI/src/mc/src-client/common/client/input/RemappingLayout.cpp
@@ -16,8 +16,3 @@ Keymapping* RemappingLayout::getKeymappingByAction(const std::string& actionName
 
     return nullptr;
 }
-
-int RemappingLayout::getAdjustedKey(int key) {
-    using function = int(__thiscall*)(RemappingLayout*, int);
-    return reinterpret_cast<function>((*reinterpret_cast<uintptr_t**>(this))[2])(this, key);
-}   
