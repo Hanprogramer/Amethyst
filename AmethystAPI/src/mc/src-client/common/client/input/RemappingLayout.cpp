b41c2cb03fe336d75f93502482120cb077215318
Raony Fernandes dos Reis <raonyreisdev@gmail.com>
2025-10-07 23:11:11 -0300

    Block stuff and stuff for hovering items

--- AmethystAPI/src/mc/src-client/common/client/input/RemappingLayout.cpp
+++ AmethystAPI/src/mc/src-client/common/client/input/RemappingLayout.cpp
@@ -1,14 +1,23 @@
 #include "mc/src-client/common/client/input/RemappingLayout.hpp"
 
-Keymapping RemappingLayout::getKeymappingByAction(const std::string* actionName) const {
+const Keymapping* RemappingLayout::getKeymappingByAction(const std::string& actionName) const {
     for (auto& keymapping : mKeymappings) {
-        if (keymapping.mAction == *actionName) return keymapping;
+        if (keymapping.mAction == actionName) return &keymapping;
     }
 
-    return Keymapping("", 0);
+    return nullptr;
+}
+
+Keymapping* RemappingLayout::getKeymappingByAction(const std::string& actionName)
+{
+    for (auto& keymapping : mKeymappings) {
+        if (keymapping.mAction == actionName) return &keymapping;
+    }
+
+    return nullptr;
 }
 
 int RemappingLayout::getAdjustedKey(int key) {
     using function = int(__thiscall*)(RemappingLayout*, int);
-    return reinterpret_cast<function>(this->vtable[2])(this, key);
+    return reinterpret_cast<function>((*reinterpret_cast<uintptr_t**>(this))[2])(this, key);
 }   
