b02547cd5cfa3bf5eb0c5ef5db64645966e5a436
Raony Fernandes dos Reis <raonyreisdev@gmail.com>
2025-10-09 13:14:42 -0300

    Cool BlockGraphics stuff

--- AmethystAPI/src/mc/src-client/common/client/renderer/block/BlockGraphics.hpp
+++ AmethystAPI/src/mc/src-client/common/client/renderer/block/BlockGraphics.hpp
@@ -26,11 +26,11 @@ class Model;
 class ResourcePackManager;
 class Experiments;
 
+/** @vptr {0x4CFAA50} */
 class BlockGraphics {
     class ModelItem;
-
 public:
-    uintptr_t** vtable;
+
     IsotropicFaceData mIsotropicFaceData;
     const Block* mBlock;
     BlockRenderLayer mRenderLayer;
@@ -39,23 +39,26 @@ public:
     float mBrightnessGamma;
     bool mFancy;
     bool mAllowSame;
-    __declspec(align(4)) std::byte mSoundType[4];
+    std::byte mSoundType[4];
     AABB mVisualShape;
     std::vector<TextureItem> mTextureItems;
-    unsigned __int64 mIconTextureIndex;
+    uint64_t mIconTextureIndex;
     std::vector<std::vector<const BlockGeometry::Model*>> mBlockModelVariants;
     std::vector<std::vector<BlockGraphics::ModelItem>> mTessellatedModelParts;
 
-public:
-    // 1.20.71.1 - 48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 4C 8B F2 48 B8
-    void setTextureItem(std::string up, std::string down, std::string north, std::string south, std::string west, std::string east);
-    
-    // 1.20.71.1 - 40 55 57 48 83 EC ? 48 8B 79
-    void setDefaultCarriedTextures();
+    /** @vidx {0} */ MC virtual ~BlockGraphics();
 
-public:
-    /// @signature {48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B E2 48 89 95 ? ? ? ? 4C 8B F9 48 89 4D}
+    /** @sig {48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 4C 8B F2 48 B8} */
+    MC void setTextureItem(std::string up, std::string down, std::string north, std::string south, std::string west, std::string east);
+    /** @sig {40 55 57 48 83 EC ? 48 8B 79} */
+    MC void setDefaultCarriedTextures();
+
+    /** @sig {48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 8B E2 48 89 95 ? ? ? ? 4C 8B F9 48 89 4D} */
     MC static void initBlocks(ResourcePackManager&, const Experiments&);
+    /** @sig {48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 44 8B FA} */
+    MC static BlockGraphics* createBlockGraphics(const HashedString& identifier, BlockShape shape);
 
-    static BlockGraphics* createBlockGraphics(const HashedString& identifier, BlockShape shape);
+    void setTextureItem(const std::string& allFaces);
 };
+
+static_assert(sizeof(BlockGraphics) == 152);
