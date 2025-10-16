4b2ef10c910c902355f003bc7570cec28f6d8507
Freddie <69014593+FrederoxDev@users.noreply.github.com>
2025-10-16 18:38:27 +0100

    misc things

--- AmethystAPI/src/mc/src-client/common/client/gui/screens/SceneStack.hpp
+++ AmethystAPI/src/mc/src-client/common/client/gui/screens/SceneStack.hpp
@@ -1,9 +1,7 @@
 /// @symbolgeneration
 #pragma once
 #include "amethyst/Imports.hpp"
-
-#include <memory>
-#include "mc/src-deps/core/utility/NonOwnerPointer.hpp"
+#include <mc/src-client/common/client/gui/screens/ScreenController.hpp>
 
 class AbstractScene;
 class SceneStack : 
@@ -14,4 +12,10 @@ public:
 
     /// @signature {48 89 54 24 ? 53 48 83 EC ? 48 8B DA C6 44 24 ? ? 0F 57 C0}
     MC void pushScreen(std::shared_ptr<AbstractScene> screen, bool idk);
+
+    /// @signature {48 8B C4 48 89 58 ? 48 89 70 ? 57 48 83 EC ? 0F 57 C0 F3 0F 7F 40}
+    MC void schedulePopScreen(uint32_t totalPopNumber);
+
+    /// @signature {89 54 24 ? 4C 8B DC 53 48 81 EC}
+    MC void popScreensBackTo(ui::SceneType sceneType);
 };
