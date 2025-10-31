bd0033e5e6e234a265afa76e97e7cdeb590b8160
TotallyTung <blakedo0602@gmail.com>
2025-10-31 20:05:48 +0700

    ActorDefinitionIdentifier ctor

--- AmethystAPI/src/mc/src/common/world/actor/ActorDefinitionIdentifier.hpp
+++ AmethystAPI/src/mc/src/common/world/actor/ActorDefinitionIdentifier.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <mc/src-deps/core/string/StringHash.hpp>
+#include <mc/src/common/world/actor/ActorType.hpp>
 
 struct ActorDefinitionIdentifier {
     std::string mNamespace;
@@ -8,4 +9,6 @@ struct ActorDefinitionIdentifier {
     std::string mInitEvent;
     std::string mFullName;
     HashedString mCanonicalName;
+	/// @signature {48 89 5C 24 ? 48 89 74 24 ? 48 89 4C 24 ? 57 48 83 EC ? 8B FA}
+    ActorDefinitionIdentifier(ActorType actorType);
 };
