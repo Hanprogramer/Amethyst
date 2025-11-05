55bc95fcb197f8284d2a3c7263988b8f3510a555
TotallyTung <blakedo0602@gmail.com>
2025-11-05 23:04:03 +0700

    Update ServerInstance.hpp

--- AmethystAPI/src/mc/src/common/server/ServerInstance.hpp
+++ AmethystAPI/src/mc/src/common/server/ServerInstance.hpp
@@ -7,7 +7,7 @@
 class IMinecraftApp;
 class Minecraft;
 class ServerNetworkSystem;
-class GameCallbacks;
+class GameCallbacks {};
 class LoopbackPacketSender;
 class Timer;
 class Scheduler;
@@ -15,7 +15,7 @@ class EducationOptions;
 class LevelStorage;
 
 namespace Core {
-	class StorageAreaStateListener;
+	class StorageAreaStateListener {};
 };
 
 class ServerInstance : public Bedrock::EnableNonOwnerReferences, public AppPlatformListener, public GameCallbacks, Core::StorageAreaStateListener {
@@ -40,9 +40,9 @@ public:
 	std::string mServerName;
 
 public:
-	/// @sig {40 53 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 48 8B DA 48 89 54 24 ? 8B 81 ? ? ? ? 90 83 F8 ? 74}
+	/// @signature {40 53 48 83 EC ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 48 8B DA 48 89 54 24 ? 8B 81 ? ? ? ? 90 83 F8 ? 74}
 	MC void _threadSafeExecute(std::function<void()> func);
 
-	/// @sig {48 89 5C 24 ? 48 89 54 24 ? 57 48 83 EC ? 48 8B FA 48 8B D9 48 C7 44 24 ? ? ? ? ? 48 8D 4C 24 ? E8 ? ? ? ? 48 8D 54 24 ? 48 8B CB E8 ? ? ? ? 90}
+	/// @signature {48 89 5C 24 ? 48 89 54 24 ? 57 48 83 EC ? 48 8B FA 48 8B D9 48 C7 44 24 ? ? ? ? ? 48 8D 4C 24 ? E8 ? ? ? ? 48 8D 54 24 ? 48 8B CB E8 ? ? ? ? 90}
 	MC void queueForServerThread(std::function<void()> func);
 };
