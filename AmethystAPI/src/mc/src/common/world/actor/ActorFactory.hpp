093a3f9ade76e1444d5423526094112abb447c49
TotallyTung <blakedo0602@gmail.com>
2025-11-07 00:00:06 +0700

    add signatures

--- AmethystAPI/src/mc/src/common/world/actor/ActorFactory.hpp
+++ AmethystAPI/src/mc/src/common/world/actor/ActorFactory.hpp
@@ -40,13 +40,15 @@ public:
 
 	void _buildSummonableActorList(const Experiments& unk0, std::function<void(const std::string&, const ActorFactoryData&)> unk1) const;
 
-	OwnerPtr<EntityContext> _constructActor(const ActorDefinitionIdentifier& unk0, const Vec3& unk1, const Vec2& unk2, const std::vector<std::string>* unk3) const;
+	/// @signature {40 55 53 56 57 41 54 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 89 4C 24}
+	MC OwnerPtr<EntityContext> _constructActor(const ActorDefinitionIdentifier& unk0, const Vec3& unk1, const Vec2& unk2, const std::vector<std::string>* unk3) const;
 
 	void _loadDefinitionGroups(ActorDefinitionGroup* unk0);
 
 	void _loadDefinitionsHelper();
 
-	bool _validateActorData(CompoundTag& unk0, ActorDefinitionIdentifier& unk1, Vec3& unk2, const LevelChunk* unk3) const;
+	/// @signature {40 55 53 56 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 0F 29 B4 24 ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 49 8B D9}
+	MC bool _validateActorData(CompoundTag& unk0, ActorDefinitionIdentifier& unk1, Vec3& unk2, const LevelChunk* unk3) const;
 
 	void applyEntityInitializer(std::unique_ptr<Actor> unk0, EntityContext& unk1) const;
 
@@ -56,8 +58,10 @@ public:
 
 	OwnerPtr<EntityContext> clientCreateDisplayActor(const ActorDefinitionIdentifier& unk0, Actor* unk1, const Vec3& unk2, const Vec2& unk3);
 
-	OwnerPtr<EntityContext> createActor(const std::string& unk0, const ActorDefinitionIdentifier& unk1, Actor* unk2, const Vec3& unk3, const Vec2& unk4);
+	/// @signature {48 89 5C 24 ? 55 56 57 41 54 41 55 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 49 8B F1 49 8B F8}
+	MC OwnerPtr<EntityContext> createActor(const std::string& unk0, const ActorDefinitionIdentifier& unk1, Actor* unk2, const Vec3& unk3, const Vec2& unk4);
 
+	/// @signature {48 8B C4 53 55 56 57 41 55 41 56 48 81 EC}
 	OwnerPtr<EntityContext> createBornActor(const ActorDefinitionIdentifier& unk0, Actor* unk1);
 
 	OwnerPtr<EntityContext> createBornActor(const ActorDefinitionIdentifier& unk0, const BlockPos& unk1);
@@ -65,25 +69,39 @@ public:
 	/// @signature {40 53 55 56 57 41 54 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 4D 8B E1 49 8B E8 48 8B DA}
 	MC OwnerPtr<EntityContext> createSpawnedActor(const ActorDefinitionIdentifier& unk0, Actor* unk1, const Vec3& unk2, const Vec2& unk3);
 
-	OwnerPtr<EntityContext> createSummonedActor(const ActorDefinitionIdentifier& unk0, Actor* unk1, const Vec3& unk2);
+	/// @signature {48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 57 48 83 EC ? 48 8B 84 24 ? ? ? ? 48 8B E9 49 8D 48}
+	MC OwnerPtr<EntityContext> createSummonedActor(const ActorDefinitionIdentifier& unk0, Actor* unk1, const Vec3& unk2);
 
-	OwnerPtr<EntityContext> createTransformedActor(const ActorDefinitionIdentifier& unk0, Actor* unk1);
+	/// @signature {40 53 55 56 57 41 56 41 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 44 24 ? 49 8B F1 49 8B E8}
+	MC OwnerPtr<EntityContext> createTransformedActor(const ActorDefinitionIdentifier& unk0, Actor* unk1);
 
 	UnknownReturn digestIdentifierListFromServer(const std::vector<ActorInfo>& unk0);
 
-	static void fillFactoryData(const ActorDefinitionIdentifier& unk0, const ActorDefinitionIdentifier& unk1, const std::unordered_map<std::string, ActorFactoryData, std::hash<std::string>, std::equal_to<std::string>, std::allocator<std::pair<const std::string, ActorFactoryData>>>& unk2, ActorFactoryData& unk3);
+	/// @signature {40 53 55 56 57 48 83 EC ? 48 8B DA}
+	MC static void fillFactoryData(const ActorDefinitionIdentifier& unk0, const ActorDefinitionIdentifier& unk1, const std::unordered_map<std::string, ActorFactoryData, std::hash<std::string>, std::equal_to<std::string>, std::allocator<std::pair<const std::string, ActorFactoryData>>>& unk2, ActorFactoryData& unk3);
 
-	static Actor* fixLegacyEntity(BlockSource& unk0, const CompoundTag* unk1);
+	/// @signature {48 89 5C 24 ? 55 56 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 48 8B DA 48 8B F9 48 85 D2}
+	MC static Actor* fixLegacyEntity(BlockSource& unk0, const CompoundTag* unk1);
 
 	UnknownReturn generateActorIdentifierList() const;
 
 	void init(const Experiments& unk0);
 
-	OwnerPtr<EntityContext> loadActor(CompoundTag* unk0, DataLoadHelper& unk1, const DimensionHeightRange& unk2, const LevelChunk* unk3);
+	/// @signature {40 55 53 56 57 41 54 41 55 41 56 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85 ? ? ? ? 4C 89 4C 24 ? 4D 8B E0 48 8B FA}
+	MC OwnerPtr<EntityContext> loadActor(CompoundTag* unk0, DataLoadHelper& unk1, const DimensionHeightRange& unk2, const LevelChunk* unk3);
 
 	OwnerPtr<EntityContext> loadActor(CompoundTag* unk0, DataLoadHelper& unk1, const DimensionHeightRange& unk2);
 
 	OwnerPtr<EntityContext> loadActor(CompoundTag* unk0, DataLoadHelper& unk1);
 
-	ActorType lookupEntityType(const ActorDefinitionIdentifier& unk0) const;
+	/// @signature {48 89 5C 24 ? 48 89 6C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 41 56 48 83 EC ? 48 8B 79 ? 48 8B 1F}
+	MC ActorType lookupEntityType(const ActorDefinitionIdentifier& unk0) const;
+
+	/// @signature {48 83 EC ? 8B 01 89 44 24}
+	MC static void registerEntityMapping(
+		ActorType const& actorType,
+		bool allowSummon,
+		void* a3,
+		std::optional<int> experimentIndex
+	);
 };
